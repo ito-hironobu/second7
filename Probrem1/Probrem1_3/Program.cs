79dbe46506a743c5366f4706d9bbb3bc1c9a43a7
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-20 17:49:06 +0900

    Probrem1_3 added

---
+++ Probrem1/Probrem1_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            s.func();
+            //s = null;
+            //GC.Collect();
+            //return;
+        }
+    }
+}
---
+++ Probrem1/Probrem1_3/Sample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem1_3
+{
+    public class Sample
+    {
+        public Sample()
+        {
+            Console.WriteLine("スタート");
+        }
+
+        public void func()
+        {
+            Console.WriteLine("func");
+        }
+
+        ~Sample()
+        {
+            Console.WriteLine("エンド");
+        }
+    }
+}
