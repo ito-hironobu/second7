5387ae3cc660882dc28d97a18e94be12279023bf
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-20 20:02:59 +0900

    Probrem2_3 added

---
+++ Probrem2/Probrem2_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem2_3
+{
+    class Program
+    {
+        static void Square(int h, int w)
+        {
+            // どちらかが0以下の時は何も表示しない
+            if(h <= 0 || w <= 0) { return; }
+
+            for (int i = 0; i < h; i++){
+                for (int j = 0; j < w; j++){
+                    Console.Write("■ ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int h = r.Next(1, 11);
+            int w = r.Next(1, 11);
+            Console.WriteLine("たて：" + h);
+            Console.WriteLine("よこ：" + w);
+            Console.WriteLine();
+
+            Square(h, w);
+        }
+    }
+}
