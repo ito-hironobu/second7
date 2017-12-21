dc45a862b4c4e53bab44355a88b8b6f378eea2ab
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-21 17:15:46 +0900

    Probrem4_2 added

---
+++ Probrem4/Probrem4_2/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem4_2
+{
+    public class Box : PlaneFigure
+    {
+        public Box()
+        {
+        }
+
+        public Box(double width, double height) : base(width, height)
+        {
+        }
+
+        public override double Area
+        {
+            get { return width * height; }
+        }
+    }
+}
---
+++ Probrem4/Probrem4_2/PlaneFigure.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem4_2
+{
+    public abstract class PlaneFigure
+    {
+        //protected double area = 0; // ←問題の指定通りにやるならいる？？
+        protected double width = 0;
+        protected double height = 0;
+
+        public PlaneFigure()
+        {
+        }
+
+        public PlaneFigure(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Width
+        {
+            set { width = value; }
+            get { return width; }
+        }
+
+        public double Height
+        {
+            set { height = value; }
+            get { return height; }
+        }
+
+        public abstract double Area
+        {
+            get;
+        }
+    }
+}
