2813015cd6fd670a9e7b3eeec696b60c76f7f6d2
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-21 14:55:09 +0900

    Probrem3_2 added

---
+++ Probrem3/Probrem3_2/FundCalc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem3_2
+{
+    public class FundCalc
+    {
+        protected double number1 = 0.0;
+        protected double number2 = 0.0;
+
+        public double Number1
+        {
+            set { number1 = value; }
+            get { return number1; }
+        }
+
+        public double Number2
+        {
+            set { number2 = value; }
+            get { return number2; }
+        }
+
+        // 和を出力
+        public double Add()
+        {
+            return number1 + number2;
+        }
+
+        // 差を出力
+        public double Sub()
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ Probrem3/Probrem3_2/NewCalc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem3_2
+{
+    public class NewCalc : FundCalc
+    {
+        public double Mul()
+        {
+            return number1 * number2;
+        }
+
+        public double Div()
+        {
+            return number1 / number2;
+        }
+    }
+}
