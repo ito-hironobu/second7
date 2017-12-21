2ea84adbd8545eaa9f364941d34e8c2a71f6829b
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-21 11:56:56 +0900

    Probrem2_4 added

---
+++ Probrem2/Probrem2_4/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem2_4
+{
+    public class Counter
+    {
+        private static int totalCount = 0;
+        private int count = 0;
+
+        public void Reset()
+        {
+            totalCount -= count;
+            count = 0;
+        }
+
+        public void Increment()
+        {
+            count++;
+            totalCount++;
+        }
+
+        public static int TotalCount()
+        {
+            return totalCount;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+    }
+}
