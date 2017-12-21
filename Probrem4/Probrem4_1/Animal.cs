940f2204f17240ef291653ab3b65c04442fc5018
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-21 16:28:47 +0900

    Probrem4_1 added

---
+++ Probrem4/Probrem4_1/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem4_1
+{
+    public abstract class Animal
+    {
+        protected string name = "";
+
+        public abstract void Bark();
+
+        public abstract void Move();
+
+        public void ShowName()
+        {
+            Console.WriteLine("名前：{0}", name);
+        }
+    }
+}
