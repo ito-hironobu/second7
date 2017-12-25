67ebda459dc66e66714a59243042a7082821e834
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-25 14:05:53 +0900

    Probrem7_3 added

---
+++ Probrem7/Probrem7_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 0, 1, 2 };
+            try{
+                for (int i = 0; i < 4; i++){
+                    Console.WriteLine("a[" + i + "]=" + a[i]);
+                }
+            }catch(IndexOutOfRangeException e){
+                Console.WriteLine("配列の範囲を越えています。");
+            }
+        }
+    }
+}
