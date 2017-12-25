2fb4f5601eade16601fd494d0bede674808d1a8f
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-12-25 13:50:01 +0900

    Probrem7_1 added

---
+++ Probrem7/Probrem7_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probrem7_1
+{
+    class Program
+    {
+        delegate void DelFunc();
+
+        static void Func1()
+        {
+            Console.WriteLine("Func1");
+        }
+
+        static void Func2()
+        {
+            Console.WriteLine("Func2");
+        }
+
+        static void Main(string[] args)
+        {
+            DelFunc df = new DelFunc(Func2);
+            df();
+        }
+    }
+}
